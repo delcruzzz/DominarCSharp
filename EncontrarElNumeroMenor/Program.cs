a370915d01d34716568243bd6e4b2dc79bf1e96e
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2024-07-29 22:33:52 -0500

    se realizaron dos ejercicios de logica

---
+++ EncontrarElNumeroMenor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EncontrarElNumeroMenor;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(FindSmallestInt(new int[] { 1, 4, -90, 2, -990, 19 }));
+    }
+
+    public static int FindSmallestInt(int[] args)
+    {
+        return args.Min();
+    }
+}
---
+++ SoyElMejorDeLaClase/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SoyElMejorDeLaClase;
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+    {
+        double classAverage = 0;
+        int sum = 0;
+
+        for(int i = 0; i < ClassPoints.Length; i++)
+        {
+            sum += ClassPoints[i];
+        }
+
+        classAverage = sum / ClassPoints.Length;
+
+        return classAverage > YourPoints ? false : true;
+    }
+}
