385059b928ca717aa97a0458e52c88c68d56380b
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2024-08-02 20:03:28 -0500

    finalización de ejercicio promedio estudiantil

---
+++ PromedioEstudiantil/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PromedioEstudiantil;
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static char GetGrade(int s1, int s2, int s3)
+    {
+        var promedio = (s1 + s2 + s3) / 3;
+
+        if (promedio >= 90 && promedio <= 100) return 'A';
+        if (promedio >= 80 && promedio < 90) return 'B';
+        if (promedio >= 70 && promedio < 80) return 'C';
+        if (promedio >= 60 && promedio < 70) return 'D';
+        
+        return 'F';
+    }
+}
